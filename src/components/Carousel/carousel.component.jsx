5c24d2166dd159a678c47f97933a49b009c2b2ca
phucvo27 <learninge27@gmail.com>
2019-08-05 21:29:33 +0700

    completed basic carousel with plain js and one component per slide

--- src/components/Carousel/carousel.component.jsx
+++ src/components/Carousel/carousel.component.jsx
@@ -4,60 +4,77 @@ import './carousel.style.scss';
 
 class Carousel extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            currenSlide: 1,
+            slides: [1,2,3],
+            isRunningAnimate: false
+        }
+    }
+
     componentDidMount(){
         // add event listener for remove class when animation run completely
         document.querySelectorAll('.slide').forEach(el => {
             el.addEventListener('animationend', this.handleRemoveClass)
         })
     }
+    componentWillUnmount(){
+        document.querySelectorAll('.slide').forEach(el => {
+            el.removeEventListener('animationend', this.handleRemoveClass)
+        })
+    }
     handleRemoveClass = (e)=>{
-        console.log('running listener')
+        
         if(e.target.classList.value.includes('removeActiveNext')){
-            console.log('prepare for removing')
-            e.target.classList.remove('removeActiveNext')
-        }
-        if(e.target.classList.value.includes('removeActivePrev')){
-            console.log('prepare for removing')
             e.target.classList.value = 'slide'
         }
+        if(e.target.classList.value.includes('removeActivePrev')){
+            e.target.classList.value = 'slide'
+        }
+        this.setState(()=>({isRunningAnimate: false}))
     }
     handleNextSlide = ()=>{
-        const currenSlide = document.querySelector('.slide.active');
-        currenSlide.classList.remove('active','activeNext');
-        currenSlide.classList.add('removeActiveNext');
-        const nextSlide = currenSlide.nextSibling;
-        console.log(nextSlide)
-        if(nextSlide){
-            nextSlide.classList.add('active','activeNext');
-        }else{
-            console.log('in else')
-            console.log(document.querySelector('.slide:first-child'));
+        this.setState(()=>({isRunningAnimate: true}))
+        try{
+            const currenSlide = document.querySelector('.slide.active');
+            currenSlide.classList.remove('active','activeNext');
+            currenSlide.classList.add('removeActiveNext');
+            const nextSlide = currenSlide.nextSibling;
+            if(nextSlide){
+                nextSlide.classList.add('active','activeNext');
+            }else{
+                document.querySelector('.slide:first-child').classList.add('active','activeNext')
+            }
+        }catch(e){
             document.querySelector('.slide:first-child').classList.add('active','activeNext')
         }
         
+        
     }
     handlePrevSlide = ()=>{
-        const currenSlide = document.querySelector('.slide.active');
-        currenSlide.classList.remove('active','activePrev');
-        // currenSlide.classList.remove('activeNext');
-        // currenSlide.classList.remove('activePrev');
-        currenSlide.classList.add('removeActivePrev');
-        const nextSlide = currenSlide.previousSibling;
-        //nextSlide.classList.add('active');
-        if(nextSlide){
-            nextSlide.classList.add('active','activePrev');
-        }else{
-            console.log('in else')
-            console.log(document.querySelector('.slide:last-child'));
+        this.setState(()=>({isRunningAnimate: true}))
+        try{
+            const currenSlide = document.querySelector('.slide.active');
+            currenSlide.classList.remove('active','activePrev');
+            currenSlide.classList.add('removeActivePrev');
+            const nextSlide = currenSlide.previousSibling;
+            if(nextSlide){
+                nextSlide.classList.add('active','activePrev');
+            }else{
+                document.querySelector('.slide:last-child').classList.add('active','activePrev')
+            }
+        }catch(e){
             document.querySelector('.slide:last-child').classList.add('active','activePrev')
         }
+        
     }
 
     render(){
         return(
             <div className="carousel">
                 <div className="slider">
-                    <div className="slide active" style={{backgroundColor: '#ddd'}}>
+                    <div className="slide active activeNext" style={{backgroundColor: '#ddd'}}>
                         <p>Slide 1 </p>
                     </div>
                     <div className="slide" style={{backgroundColor: 'goldenrod'}} >
@@ -67,10 +84,10 @@ class Carousel extends React.Component{
                         <p>Slide 3 </p>
                     </div>
                 </div>
-                <button onClick={this.handlePrevSlide} className="btn--prev">
+                <button disabled={this.state.isRunningAnimate} onClick={this.handlePrevSlide} className="btn--prev">
                     Prev
                 </button>
-                <button onClick={this.handleNextSlide} className="btn--next">
+                <button disabled={this.state.isRunningAnimate ? true : false} onClick={this.handleNextSlide} className="btn--next">
                     Next
                 </button>
             </div>
